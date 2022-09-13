ac7d033295d4a361ed6d18fa49c01642c40dc7fa
tasukabe <90055562+tasukabe@users.noreply.github.com>
2022-09-13 18:26:20 +0600

    Update Graphics_2D.cpp

--- T-graphicS/Functions/Graphics_2D.cpp
+++ T-graphicS/Functions/Graphics_2D.cpp
@@ -30,14 +30,14 @@ namespace Graphics {
                 float *X_Axis = new float[20];
                 float *Y_Axis = new float[20];
 
-            for (int i = 0; i <= Number_of_Dots; i++) {
+            for (int i = 1; i <= Number_of_Dots; i++) {
 
                 cout<<"Enter dote axiss and \ntargeted character"<<i<<"'s [x, y, {char}] = ";
                 cin>>X_Axis[i]>>Y_Axis[i]>>Targeted_char[i];
                 
                 if (!X_Axis[i] || !Y_Axis[i] || !Targeted_char[i]) { throw 1002; }
 
-                for (int I = 0; I <= Number_of_Dots; I++) {
+                for (int I = 1; I <= Number_of_Dots; I++) {
                     if (Y_Axis[i] > this->Y_Axis[I])
                         { this->Y_Axis[I] = Y_Axis[i]; this->X_Axis[I] = X_Axis[i];}
                 }
@@ -57,19 +57,27 @@ namespace Graphics {
     }
 
     void Graphics_2D :: Connection() {
-        cout<<"Enter relations between dots\n"
-            <<"like: A(1,0), B(6,0). Command is 'AB'\n"
-            <<"A@####@B";
+        cout<<"Enter relations between dots\n";
         cin>>COMMAND;
     }
 
     void Graphics_2D :: Draw() {
-        bool XY_t;
+        bool XY_t ;
 
         for (int i = 0; ; i++) {
-            cout<<"        ";
-            if (i == 0) { cout<<"Y|"<<endl; XY_t = true; continue;}
-            if (XY_t) {cout<<"|";}
+            if (i == 0) {
+                cout<<"        Y|"<<endl; XY_t = true; continue;
+            }
+            if (XY_t) {
+                cout<<"         |";
+            }
+            if (!XY_t) {
+                cout<<"    -----+";
+                for (float i = 3.34; i < X_Axis[1]; i++)
+                { cout<<"-";}
+                continue;
+            }
+            if(XY_t);
         }
     }
 }
